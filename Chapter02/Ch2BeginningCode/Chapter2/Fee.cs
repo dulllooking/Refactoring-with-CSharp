e0530909c857df34b660a33f122226afd91852a3
Ulysses <dulllooking@hotmail.com>
2024-04-09 00:02:32 +0800

    2.2.4 導入parameter 手動

--- Chapter02/Ch2BeginningCode/Chapter2/Fee.cs
+++ Chapter02/Ch2BeginningCode/Chapter2/Fee.cs
@@ -3,15 +3,9 @@
 public class Fee {
   public decimal Total { get; set; }
 
-  //收取隨身行李費
-  public void ChargeCarryOnBaggageFee(decimal fee) {
-    Console.WriteLine($"Carry-on Fee: {fee}");
-    Total += fee;
-  }
-
   //收取託運行李費
-  public void ChargeCheckedBaggageFee(decimal fee) {
-    Console.WriteLine($"Checked Fee: {fee}");
+  public void ChargeFee(decimal fee, string chargeName) {
+    Console.WriteLine($"{chargeName}: {fee}");
     Total += fee;
   }
 }
